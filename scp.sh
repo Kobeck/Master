63912f37c0f16716cb4a1ddacaeee725d8dda0fc
kobeck <konstantin.beck@uni-konstanz.de>
2020-11-26 10:25:48 +0100

    Aufgeräumt, .gitignore aktualisiert

--- scp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "file to copy"
-read FILE
-cp -r $FILE /home/watt/kobeck/COPY
-
--- test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-NOW=$(date +"%m-%d-%Y")
-FILE="backup.$NOW.tar.gz"
-echo "Backing up data to /nas42/backup.$NOW.tar.gz file, please wait..."
