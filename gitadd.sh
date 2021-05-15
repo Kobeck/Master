efffaadbb9609fe0d9db41b67be33633580b63fa
kobeck <konstantin.beck@uni-konstanz.de>
2021-05-15 20:30:36 +0200

    gitadd.sh adds all files under 90M

---
+++ gitadd.sh
@@ -0,0 +1,5 @@
+
+#find * -size +90M | sed 's|^\./||g' >> .gitignore; awk '!NF || !seen[$0]++' .gitignore
+
+#find * -size +90 -type f -print >> .gitignore
+find * -size -50M -type f -print | xargs git add
