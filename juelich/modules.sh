a058c2c7bf821614c25d5f4cf054ee1735988fea
Konstantin Beck <beck8@jwlogin02.juwels>
2021-05-17 17:38:54 +0200

    neue jobscripte auf juelich

---
+++ juelich/modules.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# load modules for LAMMPS/24_12_2020
+# Needed Modules are
+# GCC/9.3.0  ParaStationMPI/5.4.7-1
+
+module load GCC/9.3.0
+module load ParaStationMPI/5.4.7-1
+module load LAMMPS/24Dec2020
