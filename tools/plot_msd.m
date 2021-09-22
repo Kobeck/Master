08e015c82e11cf306c1a09ca2b7631798685bbf6
kobeck <konstantin.beck@uni-konstanz.de>
2021-09-22 15:38:31 +0200

    minor changes

--- tools/plot_msd.m
+++ tools/plot_msd.m
@@ -38,7 +38,7 @@ dump = readtable('dump.msd', opts);
 clear opts
 
 %% plot msd 
-Natoms = 96;
+Natoms = 125;
 steps = floor(height(dump(:,1))/(Natoms+1));
 data = zeros(steps,Natoms);
 ave = zeros(steps,1);
